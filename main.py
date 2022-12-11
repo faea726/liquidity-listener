8a88c67ac11761924c3cb9a287a6273ff830dba1
faea726 <fantasy.eagle96@gmail.com>
2022-12-12 02:08:12 +0700

    class

--- main.py
+++ main.py
@@ -4,14 +4,33 @@ import requests
 from web3 import Web3
 
 
-def main():
-    cfg = Config("conf.ini")
-    print(cfg.telegram.api_token, cfg.chain.rpc)
-    w3 = Web3(Web3.HTTPProvider(cfg.chain.rpc))
-    if not w3.isConnected():
-        return
+class LiqudityListener:
+    def __init__(self, config_file: str):
+        self.config = Config(config_file)
 
-    # send_to_telegram(api_token, channel_id, "gm")
+    def run(self):
+        print(self.config.telegram.api_token, self.config.chain.rpc)
+        self.w3 = Web3(Web3.HTTPProvider(self.config.chain.rpc))
+        if not self.w3.isConnected():
+            exit(f"Connection:", self.w3.isConnected)
+
+    def __create_contract(web3_provider: Web3, adr, abi):
+        """Crate interactable contract"""
+        try:
+            return web3_provider.eth.contract(address=adr, abi=abi)
+        except Exception as e:
+            print(e)
+            exit()
+
+    def __send_to_telegram(bot_token, chat_id="me", message=""):
+        api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+        try:
+            rsp = requests.post(api_url, json={"chat_id": chat_id, "text": message})
+            if rsp.status_code != 200:
+                print("[-]", rsp.json()["description"])
+        except Exception as err:
+            print("[-]", err)
 
 
 class Pairs:
@@ -55,25 +74,4 @@ class Chain:
         self.factory_address = factory_address
 
 
-def create_contract(web3_provider: Web3, adr, abi):
-    """Crate interactable contract"""
-    try:
-        return web3_provider.eth.contract(address=adr, abi=abi)
-    except Exception as e:
-        print(e)
-        exit()
-
-
-def send_to_telegram(bot_token, chat_id="me", message=""):
-    api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-
-    try:
-        rsp = requests.post(api_url, json={"chat_id": chat_id, "text": message})
-        if rsp.status_code != 200:
-            print("[-]", rsp.json()["description"])
-    except Exception as err:
-        print("[-]", err)
-
-
-if __name__ == "__main__":
-    main()
+LiqudityListener("conf.ini").run()
